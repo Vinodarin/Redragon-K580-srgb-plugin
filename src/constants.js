e26739c94dfc6f289aa15cca2f3aac3e96dd7ca5
Vinodarin <Vinodarin@gmail.com>
2026-04-23 18:56:28 +0300

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const VID = 0x0C45;   // пример, заменим позже
+export const PID = 0x652F;   // пример, заменим позже
+
+export const PACKET_SIZE = 56; // уточним после анализа
+export const ENDPOINT = 0x02;  // уточним после анализа
