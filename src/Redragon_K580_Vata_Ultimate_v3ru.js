c272f5407a96df5c0f005f402639b12d83a33778
Vinodarin <Vinodarin@gmail.com>
2026-05-06 21:27:43 +0300

    Update Redragon_K580_Vata_Ultimate_v3ru.js

--- src/Redragon_K580_Vata_Ultimate_v3ru.js
+++ src/Redragon_K580_Vata_Ultimate_v3ru.js
@@ -52,7 +52,6 @@ class deviceLibrary {
 }
 
 const EVISIONdeviceLibrary = new deviceLibrary();
-}
 
 // ==================================================================
 // ====== DeviceController ====== Класс управления устройством ======
